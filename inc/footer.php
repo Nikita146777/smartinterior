aa961ff92af9041863ccb3946dac5811e8e1a18d
Nikita146777 <g6394gwy@gmail.ru>
2026-09-16 07:42:06 +0300

    Автоматическая смена темы

--- inc/footer.php
+++ inc/footer.php
@@ -3,6 +3,7 @@
             <div class="container footer__container">
                 <div class="footer__logo">Smart<span>Interior</span></div>
                 <div class="footer__copy">© <?php echo date('Y'); ?> SmartInterior. Все права защищены. Челябинск | Магнитогорск</div>
+                //Тоже для комментариев
             </div>
         </footer>
     </div>
